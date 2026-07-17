75048cebb2f0da891af9bd1b47f0f3ef63707437
wensonjabi-p <wensonjabi@gmail.com>
2026-07-17 13:35:29 +0900

    Delete api/admin-logout.js

--- api/admin-logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require('./_lib');
-
-module.exports = async (req, res) => {
-  res.setHeader('Set-Cookie', clearSessionCookie());
-  res.status(200).json({ ok: true });
-};
